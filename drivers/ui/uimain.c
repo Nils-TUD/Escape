ab2ed54a1f6b7052b16cc20da113520a92716c1e
Nils Asmussen <nils@script-solution.de>
2010-04-11 11:41:59 +0000

    Set keyword Id

--- drivers/ui/uimain.c
+++ drivers/ui/uimain.c
@@ -1,5 +1,5 @@
 /**
- * $Id: vesamain.c 596 2010-04-05 13:27:47Z nasmussen $
+ * $Id$
  * Copyright (C) 2008 - 2009 Nils Asmussen
  *
  * This program is free software; you can redistribute it and/or

2eda956a0e44b6d050014ef5800a29a565c64252
Nils Asmussen <nils@script-solution.de>
2010-07-12 12:24:51 +0000

    Added a download-script for the cross-compiler and docs

---
+++ cross/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget http://ftp.gnu.org/gnu/binutils/binutils-2.20.tar.bz2
+wget http://ftp.gnu.org/gnu/gcc/gcc-4.4.3/gcc-core-4.4.3.tar.bz2
+wget http://ftp.gnu.org/gnu/gcc/gcc-4.4.3/gcc-g++-4.4.3.tar.bz2

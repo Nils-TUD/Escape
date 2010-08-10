7255738b29709913e6244e98d3203a22273d599b
Nils Asmussen <nils@script-solution.de>
2010-08-10 15:21:17 +0000

    Added newlib to dl-script

--- cross/download.sh
+++ cross/download.sh
@@ -2,3 +2,4 @@
 wget http://ftp.gnu.org/gnu/binutils/binutils-2.20.tar.bz2
 wget http://ftp.gnu.org/gnu/gcc/gcc-4.4.3/gcc-core-4.4.3.tar.bz2
 wget http://ftp.gnu.org/gnu/gcc/gcc-4.4.3/gcc-g++-4.4.3.tar.bz2
+wget ftp://sources.redhat.com/pub/newlib/newlib-1.15.0.tar.gz

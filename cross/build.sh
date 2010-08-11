681ba29be72bb23547ac5957b6767b310db3f4d0
Nils Asmussen <nils@script-solution.de>
2010-08-11 14:05:55 +0000

    Building the cross-compiler seems to work fine now :)

--- cross/build.sh
+++ cross/build.sh
@@ -64,58 +64,57 @@ ln -sf $DIST/bin/$TARGET-gcc $DIST/bin/$TARGET-cc
 make all-target-libgcc
 make install-target-libgcc
 
-#if [ 0 ]; then
-	# newlib
+# newlib
+cd $ROOT
+if [ $REBUILD -eq 1 -o ! -d $SRC/newlib ]; then
+	rm -Rf $SRC/newlib
+	cat $NEWLIB_ARCH | gunzip | tar -C $SRC -xf -
+	mv $SRC/newlib-1.15.0 $SRC/newlib
+	patch -p0 < newlib1.diff
+	cd $SRC/newlib/newlib/libc/sys
+	autoconf
 	cd $ROOT
-	if [ $REBUILD -eq 1 -o ! -d $SRC/newlib ]; then
-		rm -Rf $SRC/newlib
-		cat $NEWLIB_ARCH | gunzip | tar -C $SRC -xf -
-		mv $SRC/newlib-1.15.0 $SRC/newlib
-		patch -p0 < newlib1.diff
-		cd $SRC/newlib/newlib/libc/sys
-		autoconf
-		cd $ROOT
-		patch -p0 < newlib2.diff
-		SYSCALLS_FILE=$SRC/newlib/newlib/libc/sys/escape/syscalls.S
-		cat $LIBC/syscalls.s > $SYSCALLS_FILE
-		echo 'SYSC_VOID_1ARGS _exit,$SYSCALL_EXIT' >> $SYSCALLS_FILE
-		echo 'SYSC_VOID_1ARGS close,$SYSCALL_CLOSE' >> $SYSCALLS_FILE
-		echo 'SYSC_RET_2ARGS_ERR exec,$SYSCALL_EXEC' >> $SYSCALLS_FILE
-		echo 'SYSC_RET_0ARGS_ERR fork,$SYSCALL_FORK' >> $SYSCALLS_FILE
-		echo 'SYSC_RET_2ARGS_ERR fstat,$SYSCALL_FSTAT' >> $SYSCALLS_FILE
-		echo 'SYSC_RET_0ARGS getpid,$SYSCALL_PID' >> $SYSCALLS_FILE
-		echo 'SYSC_RET_1ARGS_ERR isatty,$SYSCALL_ISTERM' >> $SYSCALLS_FILE
-		echo 'SYSC_RET_3ARGS_ERR sendSignalTo,$SYSCALL_SENDSIG' >> $SYSCALLS_FILE
-		echo 'SYSC_RET_2ARGS_ERR _stat,$SYSCALL_STAT' >> $SYSCALLS_FILE
-		echo 'SYSC_RET_2ARGS_ERR link,$SYSCALL_LINK' >> $SYSCALLS_FILE
-		echo 'SYSC_RET_3ARGS_ERR seek,$SYSCALL_SEEK' >> $SYSCALLS_FILE
-		echo 'SYSC_RET_2ARGS_ERR open,$SYSCALL_OPEN' >> $SYSCALLS_FILE
-		echo 'SYSC_RET_3ARGS_ERR read,$SYSCALL_READ' >> $SYSCALLS_FILE
-		echo 'SYSC_RET_1ARGS_ERR _changeSize,$SYSCALL_CHGSIZE' >> $SYSCALLS_FILE
-		echo 'SYSC_RET_1ARGS_ERR unlink,$SYSCALL_UNLINK' >> $SYSCALLS_FILE
-		echo 'SYSC_RET_1ARGS_ERR waitChild,$SYSCALL_WAITCHILD' >> $SYSCALLS_FILE
-		cd $SRC/newlib/newlib/libc/sys
-		autoconf
-		cd escape
-		autoreconf
-	fi
+	patch -p0 < newlib2.diff
+	SYSCALLS_FILE=$SRC/newlib/newlib/libc/sys/escape/syscalls.S
+	cat $LIBC/syscalls.s > $SYSCALLS_FILE
+	echo 'SYSC_VOID_1ARGS _exit,$SYSCALL_EXIT' >> $SYSCALLS_FILE
+	echo 'SYSC_VOID_1ARGS close,$SYSCALL_CLOSE' >> $SYSCALLS_FILE
+	echo 'SYSC_RET_2ARGS_ERR exec,$SYSCALL_EXEC' >> $SYSCALLS_FILE
+	echo 'SYSC_RET_0ARGS_ERR fork,$SYSCALL_FORK' >> $SYSCALLS_FILE
+	echo 'SYSC_RET_2ARGS_ERR fstat,$SYSCALL_FSTAT' >> $SYSCALLS_FILE
+	echo 'SYSC_RET_0ARGS getpid,$SYSCALL_PID' >> $SYSCALLS_FILE
+	echo 'SYSC_RET_1ARGS_ERR isatty,$SYSCALL_ISTERM' >> $SYSCALLS_FILE
+	echo 'SYSC_RET_3ARGS_ERR sendSignalTo,$SYSCALL_SENDSIG' >> $SYSCALLS_FILE
+	echo 'SYSC_RET_2ARGS_ERR _stat,$SYSCALL_STAT' >> $SYSCALLS_FILE
+	echo 'SYSC_RET_2ARGS_ERR link,$SYSCALL_LINK' >> $SYSCALLS_FILE
+	echo 'SYSC_RET_3ARGS_ERR seek,$SYSCALL_SEEK' >> $SYSCALLS_FILE
+	echo 'SYSC_RET_2ARGS_ERR open,$SYSCALL_OPEN' >> $SYSCALLS_FILE
+	echo 'SYSC_RET_3ARGS_ERR read,$SYSCALL_READ' >> $SYSCALLS_FILE
+	echo 'SYSC_RET_1ARGS_ERR _changeSize,$SYSCALL_CHGSIZE' >> $SYSCALLS_FILE
+	echo 'SYSC_RET_1ARGS_ERR unlink,$SYSCALL_UNLINK' >> $SYSCALLS_FILE
+	echo 'SYSC_RET_1ARGS_ERR waitChild,$SYSCALL_WAITCHILD' >> $SYSCALLS_FILE
+	cd $SRC/newlib/newlib/libc/sys
+	autoconf
+	cd escape
+	autoreconf
+fi
 
-	cd $BUILD/newlib
-	if [ $REBUILD -eq 1 -o ! -f Makefile ]; then
-		rm -Rf $BUILD/newlib/*
-		$SRC/newlib/configure --target=$TARGET --prefix=$PREFIX
-		# to prevent problems with makeinfo
-		echo "MAKEINFO = :" >> $BUILD/newlib/Makefile
-	fi
-	make
-	make install
-#fi
+cd $BUILD/newlib
+if [ $REBUILD -eq 1 -o ! -f Makefile ]; then
+	rm -Rf $BUILD/newlib/*
+	$SRC/newlib/configure --target=$TARGET --prefix=$PREFIX
+	# to prevent problems with makeinfo
+	echo "MAKEINFO = :" >> $BUILD/newlib/Makefile
+fi
+make
+make install
 
 # libstdc++
 export PATH=$PATH:$PREFIX/bin
 mkdir -p $BUILD/gcc/libstdc++-v3
 cd $BUILD/gcc/libstdc++-v3
 if [ $REBUILD -eq 1 -o ! -f Makefile ]; then
+	# pretend that we're using newlib. without it I can't find a way to build libsupc++
 	CPP=$TARGET-cpp $SRC/gcc/libstdc++-v3/configure --host=$TARGET --prefix=$PREFIX \
 		--disable-hosted-libstdcxx --disable-nls --with-newlib
 fi

852625b229ed78ea37d88a7afc838f17c1f89b0b
Nils Asmussen <nils@script-solution.de>
2012-12-22 21:07:49 +0100

    Removed old build-system stuff

--- source/sysdeps.mk
+++
@@ -1,7 +0,0 @@
-DIST=$(ROOT)/../../escape/toolchain
-DEP_START = $(wildcard $(DIST)/$(ARCH)/lib/gcc/$(TARGET)/$(GCCVER)/crt*.o)
-ifeq ($(ARCH),i586)
-	DEP_DEFLIBS = $(DIST)/$(ARCH)/lib/libsupc++.a
-else
-	DEP_DEFLIBS =
-endif

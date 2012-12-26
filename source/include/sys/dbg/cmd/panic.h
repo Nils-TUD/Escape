ae2346dad8b1e19e0dbdab845d8a7a6688ea2792
Nils Asmussen <nils@script-solution.de>
2012-12-27 00:27:49 +0100

    Corrected license headers

--- source/include/sys/dbg/cmd/panic.h
+++ source/include/sys/dbg/cmd/panic.h
@@ -1,17 +1,20 @@
-/*
- * Copyright (C) 2012, Nils Asmussen <nils@os.inf.tu-dresden.de>
- * Economic rights: Technische Universitaet Dresden (Germany)
+/**
+ * $Id$
+ * Copyright (C) 2008 - 2011 Nils Asmussen
  *
- * This file is part of NRE (NOVA runtime environment).
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * NRE is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * NRE is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License version 2 for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #ifndef DBG_PANIC_H_

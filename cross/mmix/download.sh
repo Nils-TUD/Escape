b9a130d3896d880aea389e8e512d7c43f03e6d49
Nils Asmussen <nils@script-solution.de>
2013-01-13 12:13:37 +0100

    Removed old files for cross-compiler-build.

--- cross/mmix/download.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget -c http://ftp.gnu.org/gnu/binutils/binutils-2.21.tar.bz2
-wget -c http://ftp.gnu.org/gnu/gcc/gcc-4.6.0/gcc-core-4.6.0.tar.bz2
-wget -c http://ftp.gnu.org/gnu/gcc/gcc-4.6.0/gcc-g++-4.6.0.tar.bz2
-wget -c ftp://sources.redhat.com/pub/newlib/newlib-1.19.0.tar.gz

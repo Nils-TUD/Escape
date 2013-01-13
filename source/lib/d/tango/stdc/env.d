e6001efa1dc34e7e875919a215c99a7ec0a57491
Nils Asmussen <nils@script-solution.de>
2013-01-13 12:32:28 +0100

    Removed d-stuff. It's not working atm and we don't need it.

--- source/lib/d/tango/stdc/env.d
+++
@@ -1,8 +0,0 @@
-module tango.stdc.env;
-
-extern (C)
-{
-	extern bool getenvito(char *name,uint nameSize,uint index);
-	extern bool getenvto(char *value,uint valSize,char *name);
-	extern int setenv(char *name,char *value);
-}

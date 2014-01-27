6b03e069afa1758072b3ac19d412c53f41f0eb72
Nils Asmussen <nils@script-solution.de>
2014-01-27 13:26:02 +0100

    Added sleep application.

---
+++ source/user/sleep/sleepmain.c
@@ -0,0 +1,43 @@
+/**
+ * $Id$
+ * Copyright (C) 2008 - 2011 Nils Asmussen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <esc/common.h>
+#include <esc/cmdargs.h>
+#include <esc/thread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void usage(const char *name) {
+	fprintf(stderr,"Usage:_%s <secs>\n",name);
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc,const char **argv) {
+	int secs;
+	int res = ca_parse(argc,argv,0,"=d*",&secs);
+	if(res < 0) {
+		fprintf(stderr,"Invalid arguments: %s\n",ca_error(res));
+		usage(argv[0]);
+	}
+	if(ca_hasHelp())
+		usage(argv[0]);
+
+	sleep(secs * 1000);
+	return 0;
+}

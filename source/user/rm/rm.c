87b5744742ed00220fc0b3dc3980a9a47e5a7782
Nils Asmussen <nils@script-solution.de>
2014-05-11 12:23:58 +0200

    Added -r option to rm.

--- source/user/rm/rm.c
+++ source/user/rm/rm.c
@@ -24,26 +24,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static void removeRec(const char *path,bool rec) {
+	if(isdir(path)) {
+		if(!rec) {
+			printe("Omitting directory '%s'",path);
+			return;
+		}
+
+		char tmp[MAX_PATH_LEN];
+		DIR *dir = opendir(path);
+		sDirEntry e;
+		while(readdir(dir,&e)) {
+			if(strcmp(e.name,".") == 0 || strcmp(e.name,"..") == 0)
+				continue;
+
+			snprintf(tmp,sizeof(tmp),"%s/%s",path,e.name);
+			removeRec(tmp,rec);
+		}
+		closedir(dir);
+
+		if(rmdir(path) < 0)
+			printe("Unable to remove directory '%s'",path);
+	}
+	else if(unlink(path) < 0)
+		printe("Unable to unlink '%s'",path);
+}
+
 static void usage(const char *name) {
-	fprintf(stderr,"Usage: %s <path> ...\n",name);
+	fprintf(stderr,"Usage: %s [-r] <path> ...\n",name);
+	fprintf(stderr,"    -r:    remove directories recursively\n");
 	exit(EXIT_FAILURE);
 }
 
 int main(int argc,const char *argv[]) {
+	int rec = false;
 	const char **args;
 
-	int res = ca_parse(argc,argv,0,"");
+	int res = ca_parse(argc,argv,0,"r",&rec,&rec);
 	if(res < 0) {
 		printe("Invalid arguments: %s",ca_error(res));
 		usage(argv[0]);
 	}
-	if(ca_hasHelp())
+	if(ca_hasHelp() || ca_getFreeCount() == 0)
 		usage(argv[0]);
 
 	args = ca_getFree();
 	while(*args) {
-		if(unlink(*args) < 0)
-			printe("Unable to remove '%s'",*args);
+		removeRec(*args,rec);
 		args++;
 	}
 	return EXIT_SUCCESS;

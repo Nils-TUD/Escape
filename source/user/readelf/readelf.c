e94f108823fba7cd11bc7120dd7aba1d77417f1a
Nils Asmussen <nils@script-solution.de>
2014-07-06 10:52:25 +0200

    readelf: support x86_64 (especially relocations).

--- source/user/readelf/readelf.c
+++ source/user/readelf/readelf.c
@@ -28,12 +28,21 @@
 #if ELF_TYPE == 64
 #	define POFFW	"16"
 #	define PADDRW	"16"
+#	define INFOW	"12"
+#	define SYMVALW	"16"
 #else
 #	define POFFW	"8"
 #	define PADDRW	"8"
+#	define INFOW	"8"
+#	define SYMVALW	"8"
 #endif
 
 #define MAX_SYM_LEN		40
+
+typedef struct {
+	const char *name;
+	ulong tag;
+} sDynType;
 
 static const char *fileTypes[] = {
 	/* ET_NONE */			"NONE (No file type)",
@@ -74,41 +83,44 @@ static const char *progTypes[] = {
 	/* PT_TLS */			"TLS",
 };
 
-static const char *relocTypes[] = {
-	/* DT_NULL */			"(NULL)",
-	/* DT_NEEDED */			"(NEEDED)",
-	/* DT_PLTRELSZ */		"(PLTRELSZ)",
-	/* DT_PLTGOT */			"(PLTGOT)",
-	/* DT_HASH */			"(HASH)",
-	/* DT_STRTAB */			"(STRTAB)",
-	/* DT_SYMTAB */			"(SYMTAB)",
-	/* DT_RELA */			"(RELA)",
-	/* DT_RELASZ */			"(RELASZ)",
-	/* DT_RELAENT */		"(RELAENT)",
-	/* DT_STRSZ */			"(STRSZ)",
-	/* DT_SYMENT */			"(SYMENT)",
-	/* DT_INIT */			"(INIT)",
-	/* DT_FINI */			"(FINI)",
-	/* DT_SONAME */			"(SONAME)",
-	/* DT_RPATH */			"(RPATH)",
-	/* DT_SYMBOLIC */		"(SYMBOLIC)",
-	/* DT_REL */			"(REL)",
-	/* DT_RELSZ */			"(RELSZ)",
-	/* DT_RELENT */			"(RELENT)",
-	/* DT_PLTREL */			"(PLTREL)",
-	/* DT_DEBUG */			"(DEBUG)",
-	/* DT_TEXTREL */		"(TEXTREL)",
-	/* DT_JMPREL */			"(JMPREL)",
-	/* DT_BIND_NOW */		"(BIND_NOW)",
-	/* DT_INIT_ARRAY */		"(INIT_ARRAY)",
-	/* DT_FINI_ARRAY */		"(FINI_ARRAY)",
-	/* DT_INIT_ARRAYSZ */	"(INIT_ARRAYSZ)",
-	/* DT_FINI_ARRAYSZ */	"(FINI_ARRAYSZ)",
-	/* DT_RUNPATH */		"(RUNPATH)",
-	/* DT_FLAGS */			"(FLAGS)",
-	/* DT_ENCODING */		"(ENCODING)",
-	/* DT_PREINIT_ARRAY */	"(PREINIT_ARRAY)",
-	/* DT_PREINIT_ARRAYSZ */"(PREINIT_ARRAYSZ)",
+static sDynType dynTypes[] = {
+	{"(NULL)",				DT_NULL},
+	{"(NEEDED)",			DT_NEEDED},
+	{"(PLTRELSZ)",			DT_PLTRELSZ},
+	{"(PLTGOT)",			DT_PLTGOT},
+	{"(HASH)",				DT_HASH},
+	{"(STRTAB)",			DT_STRTAB},
+	{"(SYMTAB)",			DT_SYMTAB},
+	{"(RELA)",				DT_RELA},
+	{"(RELASZ)",			DT_RELASZ},
+	{"(RELAENT)",			DT_RELAENT},
+	{"(STRSZ)",				DT_STRSZ},
+	{"(SYMENT)",			DT_SYMENT},
+	{"(INIT)",				DT_INIT},
+	{"(FINI)",				DT_FINI},
+	{"(SONAME)",			DT_SONAME},
+	{"(RPATH)",				DT_RPATH},
+	{"(SYMBOLIC)",			DT_SYMBOLIC},
+	{"(REL)",				DT_REL},
+	{"(RELSZ)",				DT_RELSZ},
+	{"(RELENT)",			DT_RELENT},
+	{"(PLTREL)",			DT_PLTREL},
+	{"(DEBUG)",				DT_DEBUG},
+	{"(TEXTREL)",			DT_TEXTREL},
+	{"(JMPREL)",			DT_JMPREL},
+	{"(BIND_NOW)",			DT_BIND_NOW},
+	{"(INIT_ARRAY)",		DT_INIT_ARRAY},
+	{"(FINI_ARRAY)",		DT_FINI_ARRAY},
+	{"(INIT_ARRAYSZ)",		DT_INIT_ARRAYSZ},
+	{"(FINI_ARRAYSZ)",		DT_FINI_ARRAYSZ},
+	{"(RUNPATH)",			DT_RUNPATH},
+	{"(FLAGS)",				DT_FLAGS},
+	{"(ENCODING)",			DT_ENCODING},
+	{"(PREINIT_ARRAY)",		DT_PREINIT_ARRAY},
+	{"(PREINIT_ARRAYSZ)",	DT_PREINIT_ARRAYSZ},
+	{"(VERSYM)",			DT_VERSYM},
+	{"(RELACOUNT)",			DT_RELACOUNT},
+	{"(RELCOUNT)",			DT_RELCOUNT},
 };
 
 static const char *i386Relocs[] = {
@@ -150,6 +162,33 @@ static const char *i386Relocs[] = {
 	/* R_386_TLS_TPOFF32 */	"R_386_TLS_TPOFF32",
 };
 
+static const char *x86_64Relocs[] = {
+	/* R_X86_64_NONE */			"X86_64_NONE",
+	/* R_X86_64_64 */			"X86_64_64",
+	/* R_X86_64_PC32 */			"X86_64_PC32",
+	/* R_X86_64_GOT32 */		"X86_64_GOT32",
+	/* R_X86_64_PLT32 */		"X86_64_PLT32",
+	/* R_X86_64_COPY */			"X86_64_COPY",
+	/* R_X86_64_GLOB_DAT */		"X86_64_GLOB_DAT",
+	/* R_X86_64_JUMP_SLOT */	"X86_64_JUMP_SLOT",
+	/* R_X86_64_RELATIVE */		"X86_64_RELATIVE",
+	/* R_X86_64_GOTPCREL */		"X86_64_GOTPCREL",
+	/* R_X86_64_32 */			"X86_64_32",
+	/* R_X86_64_32S */			"X86_64_32S",
+	/* R_X86_64_16 */			"X86_64_16",
+	/* R_X86_64_PC16 */			"X86_64_PC16",
+	/* R_X86_64_8 */			"X86_64_8",
+	/* R_X86_64_PC8 */			"X86_64_PC8",
+	/* R_X86_64_DTPMOD64 */		"X86_64_DTPMOD64",
+	/* R_X86_64_DTPOFF64 */		"X86_64_DTPOFF64",
+	/* R_X86_64_TPOFF64 */		"X86_64_TPOFF64",
+	/* R_X86_64_TLSGD */		"X86_64_TLSGD",
+	/* R_X86_64_TLSLD */		"X86_64_TLSLD",
+	/* R_X86_64_DTPOFF32 */		"X86_64_DTPOFF32",
+	/* R_X86_64_GOTTPOFF */		"X86_64_GOTTPOFF",
+	/* R_X86_64_TPOFF32 */		"X86_64_TPOFF32",
+};
+
 static sElfSHeader *getSecByName(const char *name);
 static void loadShSyms(void);
 static void loadDynSection(void);
@@ -160,7 +199,9 @@ static void printProgHeaders(void);
 static const char *getProgFlags(ElfWord flags);
 static void printDynSection(void);
 static void printRelocations(void);
-static void printRelocTable(sElfRel *rel,size_t relCount);
+static bool printRelocationsOf(const char *section,int type,const char **relTypes,size_t typeCount);
+static void printRelTable(sElfRel *rel,size_t relCount,const char **relTypes,size_t typeCount);
+static void printRelaTable(sElfRela *rel,size_t relCount,const char **relTypes,size_t typeCount);
 static void readat(off_t offset,void *buffer,size_t count);
 static void usage(const char *name) {
 	fprintf(stderr,"Usage: %s [-ahlSrd] <file>\n",name);
@@ -203,7 +244,7 @@ int main(int argc,char **argv) {
 			file = argv[i];
 	}
 
-	if(file == NULL || (elfh + ph + sh + rel + dyns) == 0)
+	if(file == NULL || !(elfh || ph || sh || rel || dyns))
 		usage(argv[0]);
 	fd = open(file,IO_READ);
 	if(fd < 0)
@@ -304,9 +345,9 @@ static void printHeader(void) {
 	printf("\n");
 	printf("  %-34s %s\n","Type:",
 			eheader.e_type < ARRAY_SIZE(fileTypes) ? fileTypes[eheader.e_type] : "-Unknown-");
-	printf("  %-34s %p\n","Entry point address:",eheader.e_entry);
-	printf("  %-34s %lu (bytes into file)\n","Start of program headers:",eheader.e_phoff);
-	printf("  %-34s %lu (bytes into file)\n","Start of section headers:",eheader.e_shoff);
+	printf("  %-34s %p\n","Entry point address:",(void*)eheader.e_entry);
+	printf("  %-34s %lu (bytes into file)\n","Start of program headers:",(ulong)eheader.e_phoff);
+	printf("  %-34s %lu (bytes into file)\n","Start of section headers:",(ulong)eheader.e_shoff);
 	printf("  %-34s %zu (bytes)\n","Size of this header:",sizeof(sElfEHeader));
 	printf("  %-34s %hu (bytes)\n","Size of program headers:",eheader.e_phentsize);
 	printf("  %-34s %hu\n","Number of program headers:",eheader.e_phnum);
@@ -379,9 +420,9 @@ static void printProgHeaders(void) {
 		readat((off_t)datPtr,&pheader,sizeof(sElfPHeader));
 		printf("  %-14s %#06lx %#0"PADDRW"lx %#05lx %#05lx %3s %#04lx\n",
 				pheader.p_type < ARRAY_SIZE(progTypes) ? progTypes[pheader.p_type] : "-Unknown-",
-				pheader.p_offset,pheader.p_vaddr,pheader.p_filesz,
-				pheader.p_memsz,getProgFlags(pheader.p_flags),pheader.p_align);
-		printf("  %-23s %#0"PADDRW"lx\n","",pheader.p_paddr);
+				(ulong)pheader.p_offset,(ulong)pheader.p_vaddr,(ulong)pheader.p_filesz,
+				(ulong)pheader.p_memsz,getProgFlags(pheader.p_flags),(ulong)pheader.p_align);
+		printf("  %-23s %#0"PADDRW"lx\n","",(ulong)pheader.p_paddr);
 	}
 	printf("\n");
 }
@@ -392,6 +433,14 @@ static const char *getProgFlags(ElfWord flags) {
 	str[1] = (flags & PF_W) ? 'W' : ' ';
 	str[2] = (flags & PF_X) ? 'E' : ' ';
 	return str;
+}
+
+static sDynType *getDynType(ulong tag) {
+	for(size_t i = 0; i < ARRAY_SIZE(dynTypes); ++i) {
+		if(dynTypes[i].tag == tag)
+			return dynTypes + i;
+	}
+	return NULL;
 }
 
 static void printDynSection(void) {
@@ -411,15 +460,16 @@ static void printDynSection(void) {
 	/* print entries */
 	dwalk = dyn;
 	printf("Dynamic section at offset %#x contains %d entries:\n",dynOff,eCount);
-	printf("  %-10s %-28s %s\n","Tag","Type","Name/Value");
+	printf(" %-10s %-28s %s\n","Tag","Type","Name/Value");
 	while(dwalk->d_tag != DT_NULL) {
-		printf(" %#08x %-28s",dwalk->d_tag,
-			dwalk->d_tag < (int)ARRAY_SIZE(relocTypes) ? relocTypes[dwalk->d_tag] : "-Unknown-");
+		sDynType *relType = getDynType(dwalk->d_tag);
+		printf(" %#08x %-28s ",dwalk->d_tag,relType ? relType->name : "-Unknown-");
 		switch(dwalk->d_tag) {
 			case DT_SONAME:
 			case DT_NEEDED:
 				printf("Shared library: [%s]\n",dynstrtbl + dwalk->d_un.d_val);
 				break;
+
 			case DT_INIT:
 			case DT_FINI:
 			case DT_HASH:
@@ -428,15 +478,30 @@ static void printDynSection(void) {
 			case DT_PLTGOT:
 			case DT_JMPREL:
 			case DT_REL:
+			case DT_RELA:
+			case DT_DEBUG:
 				printf("%#x\n",dwalk->d_un.d_ptr);
 				break;
+
+			case DT_RELCOUNT:
+			case DT_RELACOUNT:
+				printf("%d\n",dwalk->d_un.d_val);
+				break;
+
 			case DT_STRSZ:
 			case DT_SYMENT:
 			case DT_PLTRELSZ:
 			case DT_RELSZ:
 			case DT_RELENT:
+			case DT_RELASZ:
+			case DT_RELAENT:
 				printf("%d (bytes)\n",dwalk->d_un.d_val);
 				break;
+
+			case DT_PLTREL:
+				printf("%s\n",dwalk->d_un.d_val == DT_REL ? "REL" : "RELA");
+				break;
+
 			default:
 				printf("?\n");
 				break;
@@ -447,50 +512,61 @@ static void printDynSection(void) {
 }
 
 static void printRelocations(void) {
-	sElfSHeader *relSec = getSecByName(".rel.dyn");
-	size_t relCount = relSec ? relSec->sh_size / sizeof(sElfRel) : 0;
-	sElfRel *rel;
-	if(relSec == NULL || relCount == 0) {
+	const char **relTypes;
+	size_t typeCount;
+	if(eheader.e_machine == EM_386) {
+		relTypes = i386Relocs;
+		typeCount = ARRAY_SIZE(i386Relocs);
+	}
+	else if(eheader.e_machine == EM_X86_64) {
+		relTypes = x86_64Relocs;
+		typeCount = ARRAY_SIZE(x86_64Relocs);
+	}
+	else {
 		printf("There are no relocations in this file.\n\n");
 		return;
 	}
 
-	/* relocations */
-	rel = (sElfRel*)malloc(relCount * sizeof(sElfRel));
-	if(!rel)
-		error("Not enough mem for relocations");
-	readat(relSec->sh_offset,rel,relCount * sizeof(sElfRel));
-	/* TODO there are different relocation-sections */
-	printf("Relocation section '.rel.dyn' at offset %#x contains %d entries:\n",
-			relSec->sh_offset,relCount);
-	printf(" %-10s %-7s %-15s %-10s %s\n","Offset","Info","Type","Sym.Value","Sym. Name");
-	printRelocTable(rel,relCount);
-	printf("\n");
-	free(rel);
-
-	/* jump-relocs */
-	relSec = getSecByName(".rel.plt");
-	relCount = relSec->sh_size / sizeof(sElfRel);
-	rel = (sElfRel*)malloc(relCount * sizeof(sElfRel));
-	if(!rel)
-		error("Not enough mem for jump-relocations");
-	readat(relSec->sh_offset,rel,relCount * sizeof(sElfRel));
-	printf("Relocation section '.rel.plt' at offset %#x contains %d entries:\n",
-			relSec->sh_offset,relCount);
-	printf(" %-10s %-7s %-15s %-10s %s\n","Offset","Info","Type","Sym.Value","Sym. Name");
-	printRelocTable(rel,relCount);
-	printf("\n");
-	free(rel);
+	bool found = false;
+	found |= printRelocationsOf(".rel.dyn",DT_REL,relTypes,typeCount);
+	found |= printRelocationsOf(".rela.dyn",DT_RELA,relTypes,typeCount);
+	found |= printRelocationsOf(".rel.plt",DT_REL,relTypes,typeCount);
+	found |= printRelocationsOf(".rela.plt",DT_RELA,relTypes,typeCount);
+	if(!found)
+		printf("There are no relocations in this file.\n\n");
 }
 
-static void printRelocTable(sElfRel *rel,size_t relCount) {
+static bool printRelocationsOf(const char *section,int type,const char **relTypes,size_t typeCount) {
+	sElfSHeader *relSec = getSecByName(section);
+	size_t relSize = type == DT_REL ? sizeof(sElfRel) : sizeof(sElfRela);
+	size_t relCount = relSec ? relSec->sh_size / relSize : 0;
+	if(relSec == NULL || relCount == 0)
+		return false;
+
+	/* relocations */
+	void *rel = malloc(relCount * relSize);
+	if(!rel)
+		error("Not enough mem for relocations");
+	readat(relSec->sh_offset,rel,relCount * relSize);
+	printf("Relocation section '%s' at offset %#x contains %d entries:\n",
+			section,relSec->sh_offset,relCount);
+	if(type == DT_REL)
+		printRelTable((sElfRel*)rel,relCount,relTypes,typeCount);
+	else
+		printRelaTable((sElfRela*)rel,relCount,relTypes,typeCount);
+	free(rel);
+	return true;
+}
+
+static void printRelTable(sElfRel *rel,size_t relCount,const char **relTypes,size_t typeCount) {
 	char symcopy[MAX_SYM_LEN];
 	size_t i;
+	printf("%-8s %-"INFOW"s %-16s %-"SYMVALW"s %s\n","Offset","Info","Type","Sym.Val.","Sym. Name");
 	for(i = 0; i < relCount; i++) {
 		uint relType = ELF_R_TYPE(rel[i].r_info);
 		uint symIndex = ELF_R_SYM(rel[i].r_info);
-		printf("%08x  %08x %-16s",rel[i].r_offset,rel[i].r_info,
-			relType < ARRAY_SIZE(i386Relocs) ? i386Relocs[relType] : "-Unknown-");
+		printf("%08lx %0"INFOW"lx %-16s",(ulong)rel[i].r_offset,(ulong)rel[i].r_info,
+			relType < typeCount ? relTypes[relType] : "-Unknown-");
 		if(symIndex != 0) {
 			sElfSym *sym = dynsyms + symIndex;
 			const char *symName = dynstrtbl + sym->st_name;
@@ -498,10 +574,36 @@ static void printRelocTable(sElfRel *rel,size_t relCount) {
 				strnzcpy(symcopy,symName,sizeof(symcopy));
 			else
 				symcopy[0] = '\0';
-			printf(" %08x   %-25s",sym->st_value,symcopy);
+			printf(" %0"SYMVALW"x %-25s",sym->st_value,symcopy);
 		}
 		printf("\n");
 	}
+	printf("\n");
+}
+
+static void printRelaTable(sElfRela *rel,size_t relCount,const char **relTypes,size_t typeCount) {
+	char symcopy[MAX_SYM_LEN];
+	size_t i;
+	printf("%-8s %-"INFOW"s %-16s %-"SYMVALW"s %s\n","Offset","Info","Type","Sym.Val.","Sym. Name + Addend");
+	for(i = 0; i < relCount; i++) {
+		uint relType = ELF_R_TYPE(rel[i].r_info);
+		uint symIndex = ELF_R_SYM(rel[i].r_info);
+		printf("%08lx %0"INFOW"lx %-16s",(ulong)rel[i].r_offset,(ulong)rel[i].r_info,
+			relType < typeCount ? relTypes[relType] : "-Unknown-");
+		if(symIndex != 0) {
+			sElfSym *sym = dynsyms + symIndex;
+			const char *symName = dynstrtbl + sym->st_name;
+			if(symName)
+				strnzcpy(symcopy,symName,sizeof(symcopy));
+			else
+				symcopy[0] = '\0';
+			printf(" %0"SYMVALW"x %s + %x",sym->st_value,symcopy,rel[i].r_addend);
+		}
+		else
+			printf(" %"SYMVALW"s %x","",rel[i].r_addend);
+		printf("\n");
+	}
+	printf("\n");
 }
 
 static void readat(off_t offset,void *buffer,size_t count) {

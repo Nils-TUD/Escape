886824f15826b72937ce11ecedadeb2a95d577cb
Nils Asmussen <nils@script-solution.de>
2015-04-11 15:51:39 +0200

    chmod: support more convenient permission specifications.

--- source/user/chmod/chmod.c
+++ source/user/chmod/chmod.c
@@ -21,37 +21,131 @@
 #include <sys/common.h>
 #include <sys/proc.h>
 #include <sys/stat.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+	USER	= 1 << 0,
+	GROUP	= 1 << 1,
+	OTHERS	= 1 << 2,
+	ALL		= USER | GROUP | OTHERS
+} Part;
+
+typedef enum {
+	ADD,
+	SUB,
+	ASSIGN
+} Operation;
+
+static bool isNumeric(const char *str) {
+	while(*str) {
+		if(!isxdigit(*str))
+			return false;
+		str++;
+	}
+	return true;
+}
+
+static mode_t changeMode(mode_t mode,Operation op,int shift,int perm) {
+	switch(op) {
+		case ADD:
+			mode |= perm << shift;
+			break;
+		case SUB:
+			mode &= ~(perm << shift);
+			break;
+		case ASSIGN:
+			mode &= ~(07 << shift);
+			mode |= perm << shift;
+			break;
+	}
+	return mode;
+}
+
+static mode_t parseMode(const char *path,const char *mode) {
+	if(isNumeric(mode))
+		return (mode_t)strtol(mode,NULL,0);
+
+	Part part = 0;
+	struct { char c; Part p; } parts[] = {
+		{'u',USER},{'g',GROUP},{'o',OTHERS},
+	};
+	while(*mode && strchr("ugo",*mode)) {
+		for(size_t i = 0; i < ARRAY_SIZE(parts); ++i) {
+			if(*mode == parts[i].c) {
+				part |= parts[i].p;
+				mode++;
+				break;
+			}
+		}
+	}
+	if(part == 0)
+		part = ALL;
+
+	Operation op = ASSIGN;
+	struct { char c; Operation op; } ops[] = {
+		{'+',ADD},{'-',SUB},{'=',ASSIGN},
+	};
+	for(size_t i = 0; i < ARRAY_SIZE(ops); ++i) {
+		if(*mode == ops[i].c) {
+			op = ops[i].op;
+			mode++;
+			break;
+		}
+	}
+
+	int perm = 0;
+	struct { char c; int perm; } perms[] = {
+		{'r',04},{'w',02},{'x',01},
+	};
+	while(*mode) {
+		for(size_t i = 0; i < ARRAY_SIZE(perms); ++i) {
+			if(*mode == perms[i].c) {
+				perm |= perms[i].perm;
+				mode++;
+				break;
+			}
+		}
+	}
+
+	mode_t imode = 0;
+	if(part != ALL || op != ASSIGN) {
+		struct stat st;
+		if(stat(path,&st) < 0) {
+			fprintf(stderr,"Unable to stat '%s'\n",path);
+			return -1;
+		}
+		imode = st.st_mode & 0777;
+	}
+
+	if(part & USER)
+		imode = changeMode(imode,op,6,perm);
+	if(part & GROUP)
+		imode = changeMode(imode,op,3,perm);
+	if(part & OTHERS)
+		imode = changeMode(imode,op,0,perm);
+	return imode;
+}
 
 static void usage(const char *name) {
 	fprintf(stderr,"Usage: %s <mode> <file>...\n",name);
+	fprintf(stderr,"  <mode> can be either numeric or of the form [ugo][+-=][rwx]\n");
 	exit(EXIT_FAILURE);
 }
 
-static mode_t parseMode(const char *mode) {
-	return (mode_t)strtol(mode,NULL,0);
-}
-
 int main(int argc,const char **argv) {
-	char *mode = NULL;
-	mode_t imode;
-	const char **args;
-
-	int res = ca_parse(argc,argv,0,"=s*",&mode);
-	if(res < 0) {
-		printe("Invalid arguments: %s",ca_error(res));
-		usage(argv[0]);
-	}
-	if(ca_hasHelp())
+	if(ca_hasHelp() || argc < 3)
 		usage(argv[0]);
 
-	imode = parseMode(mode);
-	args = ca_getFree();
-	while(*args) {
-		if(chmod(*args,imode) < 0)
-			printe("Unable to set mode of '%s' to %04o",*args,imode);
-		args++;
+	const char *mode = argv[1];
+	for(int i = 2; i < argc; ++i) {
+		mode_t imode = parseMode(argv[i],mode);
+		if(imode != (mode_t)-1) {
+			if(chmod(argv[i],imode) < 0)
+				printe("Unable to set mode of '%s' to %04o",argv[i],imode);
+		}
 	}
 	return EXIT_SUCCESS;
 }

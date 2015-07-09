26cd062c217ed8d075066b0bc774d32ba304ed48
Nils Asmussen <nils@script-solution.de>
2015-07-09 22:05:56 +0200

    Use ext2 for ramdisks and added a qemu-usb script.

--- source/boot/x86/images.sh
+++ source/boot/x86/images.sh
@@ -80,3 +80,56 @@ EOF
 	./tools/disk.py create --offset 2048 --part ext2r0 128 "$dir" --part ext2r0 4 - --part nofs 8 - "$dst" 1>&2
 	sudo rm -Rf $dir
 }
+
+create_fsimg() {
+	src="$1"
+	dst="$2"
+
+	dir=`mktemp -d`
+	cp -R $src/* $dir
+
+	sudo ./boot/perms.sh $dir
+	./tools/disk.py create --offset 0 --part ext2r0 20 "$dir" "$dst" --flat --nogrub 1>&2
+	sudo rm -Rf $dir
+}
+
+create_usbimg() {
+	create_fsimg $1/dist $1/fs.img
+
+	suffix=""
+	if [ "$ESC_TARGET" = "x86_64" ]; then
+		suffix=".elf32"
+	fi
+
+	tmp=`mktemp -d`
+
+	# copy files necessary to boot, including the iso image containing everything
+	cp -R $1/dist/boot $tmp
+	mkdir $tmp/sbin $tmp/bin
+	for d in ramdisk ext2 pci; do
+		cp $1/dist/sbin/$d $tmp/sbin
+	done
+	cp $1/dist/bin/initloader $tmp/bin
+	cp $1/fs.img $tmp/boot
+
+	# create menu.lst
+	cat > $tmp/boot/grub/menu.lst <<EOF
+default 0
+timeout 3
+
+title Escape
+kernel /boot/escape$suffix root=/dev/ext2-ramdisk-fs nosmp nolog
+module /bin/initloader
+module /sbin/ramdisk /dev/ramdisk-fs -f /sys/boot/fs.img
+module /sbin/pci /dev/pci
+module /sbin/ext2 /dev/ext2-ramdisk-fs /dev/ramdisk-fs
+module /boot/fs.img
+
+title Escape - Test
+kernel /boot/escape_test$suffix
+module /bin/initloader
+EOF
+
+	# create disk. 64 MB should be enough
+	./tools/disk.py create --offset 2048 --part ext3 64 $tmp $1/usb.img
+}

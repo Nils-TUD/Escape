89710b9e12186ebe16a7a19bdb38352cb993cd90
Nils Asmussen <nils@os.inf.tu-dresden.de>
2016-12-13 12:59:58 +0100

    Removed gaps between message ids.

--- source/include/sys/messages.h
+++ source/include/sys/messages.h
@@ -68,75 +68,75 @@ enum {
 	MSG_WIN_EVENT					= 312,	/* for all events */
 
 	/* screen */
-	MSG_SCR_SETCURSOR				= 500,	/* sets the cursor */
-	MSG_SCR_GETMODE					= 501,	/* gets information about the current video-mode */
-	MSG_SCR_SETMODE					= 502,	/* sets the video-mode */
-	MSG_SCR_GETMODES				= 503,	/* gets all video-modes */
-	MSG_SCR_UPDATE					= 504,	/* updates a part of the screen */
+	MSG_SCR_SETCURSOR				= 400,	/* sets the cursor */
+	MSG_SCR_GETMODE					= 401,	/* gets information about the current video-mode */
+	MSG_SCR_SETMODE					= 402,	/* sets the video-mode */
+	MSG_SCR_GETMODES				= 403,	/* gets all video-modes */
+	MSG_SCR_UPDATE					= 404,	/* updates a part of the screen */
 
 	/* vterm */
-	MSG_VT_GETFLAG					= 600,	/* gets a flag */
-	MSG_VT_SETFLAG					= 601,	/* sets a flag */
-	MSG_VT_BACKUP					= 602,	/* backups the screen */
-	MSG_VT_RESTORE					= 603,	/* restores the screen */
-	MSG_VT_SHELLPID					= 604,	/* gives the vterm the shell-pid */
-	MSG_VT_ISVTERM					= 605,	/* dummy message on which only vterm answers with no error */
-	MSG_VT_SETMODE					= 606,	/* requests vterm to set the video-mode */
+	MSG_VT_GETFLAG					= 500,	/* gets a flag */
+	MSG_VT_SETFLAG					= 501,	/* sets a flag */
+	MSG_VT_BACKUP					= 502,	/* backups the screen */
+	MSG_VT_RESTORE					= 503,	/* restores the screen */
+	MSG_VT_SHELLPID					= 504,	/* gives the vterm the shell-pid */
+	MSG_VT_ISVTERM					= 505,	/* dummy message on which only vterm answers with no error */
+	MSG_VT_SETMODE					= 506,	/* requests vterm to set the video-mode */
 
 	/* keyboard */
-	MSG_KB_EVENT					= 700,	/* events that the keyboard-driver sends */
+	MSG_KB_EVENT					= 600,	/* events that the keyboard-driver sends */
 
 	/* mouse */
-	MSG_MS_EVENT					= 800,	/* events that the mouse-driver sends */
+	MSG_MS_EVENT					= 700,	/* events that the mouse-driver sends */
 
 	/* UI manager */
-	MSG_UIM_GETKEYMAP				= 900,	/* gets the current keymap path */
-	MSG_UIM_SETKEYMAP				= 901,	/* sets a keymap, expects the keymap-path as argument */
-	MSG_UIM_EVENT					= 902,	/* the message-id for sending events to the listeners */
+	MSG_UIM_GETKEYMAP				= 800,	/* gets the current keymap path */
+	MSG_UIM_SETKEYMAP				= 801,	/* sets a keymap, expects the keymap-path as argument */
+	MSG_UIM_EVENT					= 802,	/* the message-id for sending events to the listeners */
 
 	/* PCI */
-	MSG_PCI_GET_BY_CLASS			= 1000,	/* searches for a PCI device with given class */
-	MSG_PCI_GET_BY_ID				= 1001,	/* searches for a PCI device with given id */
-	MSG_PCI_GET_BY_INDEX			= 1002,	/* searches for a PCI device with given index (0..n) */
-	MSG_PCI_GET_COUNT				= 1003,	/* gets the number of PCI devices */
-	MSG_PCI_READ					= 1004,	/* reads from PCI config space */
-	MSG_PCI_WRITE					= 1005,	/* writes to PCI config space */
-	MSG_PCI_HAS_CAP					= 1006,	/* checks if a given capability exists for a given device */
-	MSG_PCI_ENABLE_MSIS				= 1007,	/* enables MSIs for a given device */
+	MSG_PCI_GET_BY_CLASS			= 900,	/* searches for a PCI device with given class */
+	MSG_PCI_GET_BY_ID				= 901,	/* searches for a PCI device with given id */
+	MSG_PCI_GET_BY_INDEX			= 902,	/* searches for a PCI device with given index (0..n) */
+	MSG_PCI_GET_COUNT				= 903,	/* gets the number of PCI devices */
+	MSG_PCI_READ					= 904,	/* reads from PCI config space */
+	MSG_PCI_WRITE					= 905,	/* writes to PCI config space */
+	MSG_PCI_HAS_CAP					= 906,	/* checks if a given capability exists for a given device */
+	MSG_PCI_ENABLE_MSIS				= 907,	/* enables MSIs for a given device */
 
 	/* init */
-	MSG_INIT_REBOOT					= 1100,	/* requests a reboot */
-	MSG_INIT_SHUTDOWN				= 1101,	/* requests a shutdown */
-	MSG_INIT_IAMALIVE				= 1102,	/* tells init that the shutdown-request has been received
+	MSG_INIT_REBOOT					= 1000,	/* requests a reboot */
+	MSG_INIT_SHUTDOWN				= 1001,	/* requests a shutdown */
+	MSG_INIT_IAMALIVE				= 1002,	/* tells init that the shutdown-request has been received
 										 	 * and that you promise to terminate as soon as possible */
 
 	/* NIC */
-	MSG_NIC_GETMAC					= 1300,	/* get the MAC address of a NIC */
-	MSG_NIC_GETMTU					= 1301,	/* get the MTU of a NIC */
+	MSG_NIC_GETMAC					= 1100,	/* get the MAC address of a NIC */
+	MSG_NIC_GETMTU					= 1101,	/* get the MTU of a NIC */
 
 	/* network */
-	MSG_NET_LINK_ADD				= 1401,	/* adds a link */
-	MSG_NET_LINK_REM				= 1402,	/* removes a link */
-	MSG_NET_LINK_CONFIG				= 1403,	/* sets the ip, gateway and subnet-mask of a link */
-	MSG_NET_LINK_MAC				= 1404,	/* gets the MAC address of a link */
-	MSG_NET_ROUTE_ADD				= 1405,	/* adds an entry to the routing table */
-	MSG_NET_ROUTE_REM				= 1406,	/* removes an entry from the routing table */
-	MSG_NET_ROUTE_CONFIG			= 1407,	/* sets the status of a routing table entry */
-	MSG_NET_ROUTE_GET				= 1408,	/* gets the destination for an IP address */
-	MSG_NET_ARP_ADD					= 1409,	/* resolves an IP address and puts it into the ARP table */
-	MSG_NET_ARP_REM					= 1410,	/* removes an IP address from the ARP table */
+	MSG_NET_LINK_ADD				= 1200,	/* adds a link */
+	MSG_NET_LINK_REM				= 1201,	/* removes a link */
+	MSG_NET_LINK_CONFIG				= 1202,	/* sets the ip, gateway and subnet-mask of a link */
+	MSG_NET_LINK_MAC				= 1203,	/* gets the MAC address of a link */
+	MSG_NET_ROUTE_ADD				= 1204,	/* adds an entry to the routing table */
+	MSG_NET_ROUTE_REM				= 1205,	/* removes an entry from the routing table */
+	MSG_NET_ROUTE_CONFIG			= 1206,	/* sets the status of a routing table entry */
+	MSG_NET_ROUTE_GET				= 1207,	/* gets the destination for an IP address */
+	MSG_NET_ARP_ADD					= 1208,	/* resolves an IP address and puts it into the ARP table */
+	MSG_NET_ARP_REM					= 1209,	/* removes an IP address from the ARP table */
 
 	/* socket */
-	MSG_SOCK_CONNECT				= 1500,	/* connects a socket to the other endpoint */
-	MSG_SOCK_BIND					= 1501,	/* bind a socket to an address */
-	MSG_SOCK_LISTEN					= 1502,	/* puts a socket into listening state */
-	MSG_SOCK_RECVFROM				= 1503,	/* receive data from a socket */
-	MSG_SOCK_SENDTO					= 1504,	/* send data to a socket */
-	MSG_SOCK_ABORT					= 1505,	/* aborts the connection, i.e. sends a RST */
+	MSG_SOCK_CONNECT				= 1300,	/* connects a socket to the other endpoint */
+	MSG_SOCK_BIND					= 1301,	/* bind a socket to an address */
+	MSG_SOCK_LISTEN					= 1302,	/* puts a socket into listening state */
+	MSG_SOCK_RECVFROM				= 1303,	/* receive data from a socket */
+	MSG_SOCK_SENDTO					= 1304,	/* send data to a socket */
+	MSG_SOCK_ABORT					= 1305,	/* aborts the connection, i.e. sends a RST */
 
 	/* DNS */
-	MSG_DNS_RESOLVE					= 1600,	/* resolve a name to an address */
-	MSG_DNS_SET_SERVER				= 1601,	/* set the name server */
+	MSG_DNS_RESOLVE					= 1400,	/* resolve a name to an address */
+	MSG_DNS_SET_SERVER				= 1401,	/* set the name server */
 };
 
 static const size_t IPC_DEF_SIZE	= 256;

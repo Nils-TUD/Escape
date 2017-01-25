f7ebf9d4bbac1c23ceb3289687d3268c90e3921b
Nils Asmussen <nils@os.inf.tu-dresden.de>
2017-01-25 16:00:43 +0100

    perms.sh: ensure to chown all files and dirs.

--- source/boot/perms.sh
+++ source/boot/perms.sh
@@ -7,29 +7,27 @@ ujon=11
 gjon=12
 gshared=10
 
+# root-owned by default
+chown -R $root:$root $dir/*
+
 # /boot
-chown -R $root:$root $dir/boot
 chmod -R 0600 $dir/boot
 find $dir/boot -type d | xargs chmod 0755
 
 # /bin
-chown -R $root:$root $dir/bin
 chmod -R 0755 $dir/bin
 
 # /sbin
-chown -R $root:$root $dir/sbin
 chmod 0755 $dir/sbin
 chmod 0744 $dir/sbin/*
 
 # /lib
 if [ -d $dir/lib ]; then
-	chown -R $root:$root $dir/lib
 	chmod 0755 $dir/lib
 	chmod 0644 $dir/lib/*
 fi
 
 # /etc
-chown -R $root:$root $dir/etc
 chmod -R 0644 $dir/etc
 chmod 0755 $dir/etc
 chmod 0755 $dir/etc/keymaps
@@ -37,12 +35,10 @@ chmod 0755 $dir/etc/net
 chmod 0755 $dir/etc/themes
 
 # /etc/groups
-chown -R $root:$root $dir/etc/groups
 find $dir/etc/groups -type d | xargs chmod 0755
 find $dir/etc/groups -type f | xargs chmod 0644
 
 # /etc/users
-chown -R $root:$root $dir/etc/users
 find $dir/etc/users -type d | xargs chmod 0755
 find $dir/etc/users -type f | xargs chmod 0644
 # users can change their own password
@@ -53,12 +49,10 @@ chmod 0600 $dir/etc/users/jon/passwd
 chmod 0600 $dir/etc/users/root/passwd
 
 # /root
-chown -R $root:$root $dir/root
 chmod -R 0600 $dir/root
 find $dir/root -type d | xargs chmod +x
 
 # /home
-chown $root:$root $dir/home
 chmod 0755 $dir/home
 
 # /home/hrniels
